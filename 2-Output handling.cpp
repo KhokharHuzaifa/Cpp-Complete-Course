a3a88a6ae02c367c72a2c0fd70234a35e99c4e3d
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-05 10:57:33 +0500

    Output handling

---
+++ 2-Output handling.cpp
@@ -0,0 +1,10 @@
+// The cout object, together with the << operator, is used to output values/print text
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+//   You can add as many cout objects as you want. However, note that it does not insert a new line at the end of the output
+ cout << "I am learning C++";
+  return 0;
+}
