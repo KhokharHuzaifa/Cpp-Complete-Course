307e054f40c0475cd9512f0cd648573da7f3e99a
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-05 11:18:25 +0500

    Declaring variables

---
+++ 6-Variables/1-Declaring variables.cpp
@@ -0,0 +1,23 @@
+// To create a variable, specify the type and assign it a value:
+
+// Syntax
+// type variableName = value;
+// Example
+// Create a variable called myNum of type int and assign it the value 15:
+
+int myNum;
+myNum = 15;
+cout << myNum;
+
+// Note that if you assign a new value to an existing variable, it will overwrite the previous value:
+
+// Example
+int myNum = 15;  // myNum is 15
+myNum = 10;  // Now myNum is 10
+cout << myNum;  // Outputs 10
+
+int myNum = 5;               // Integer (whole number without decimals)
+double myFloatNum = 5.99;    // Floating point number (with decimals)
+char myLetter = 'D';         // Character
+string myText = "Hello";     // String (text)
+bool myBoolean = true;       // Boolean (true or false)
