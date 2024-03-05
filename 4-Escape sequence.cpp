b57c711c0ff1cda3877b58000f7b906be375a165
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-05 11:06:26 +0500

    Escape sequence

---
+++ 4-Escape sequence.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // To insert a new line, you can use the \n character
+  cout << "Hello World! \n";
+  cout << "I am learning C++";
+//   Another way to insert a new line, is with the endl manipulator
+    cout << "Hello World!" << endl;
+  cout << "I am learning C++";
+  return 0;
+}
+// Examples of other valid escape sequences are
+//          "\t	Creates a horizontal tab"
+//          "\\	Inserts a backslash character (\)"
+//           \" "Inserts a double quote character"
