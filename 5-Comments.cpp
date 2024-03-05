e8fb392c6c92d165793cff36851775afb8499788
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-05 11:09:42 +0500

    Comments

---
+++ 5-Comments.cpp
@@ -0,0 +1,22 @@
+// Comments can be used to explain C++ code, and to make it more readable. It can also be used to prevent execution 
+// when testing alternative code. Comments can be singled-lined or multi-lined
+
+Single-line Comments
+// Single-line comments start with two forward slashes (//).
+
+// Any text between // and the end of the line is ignored by the compiler (will not be executed).
+
+// This example uses a single-line comment before a line of code:
+
+// Example
+cout << "Hello World!"; // This is a comment
+
+C++ Multi-line Comments
+// Multi-line comments start with /* and ends with */.
+
+// Any text between /* and */ will be ignored by the compiler:
+
+// Example
+/* The code below will print the words Hello World!
+to the screen, and it is amazing */
+cout << "Hello World!";
