1769f35dfe0a941fa922e672e4809993fd5d3f17
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-06 11:24:54 +0500

    Constant varibales

---
+++ 6-Variables/3-Constants.cpp
@@ -0,0 +1,9 @@
+// When you do not want others (or yourself) to change existing variable values, use the const keyword (this will declare the variable as "constant", 
+// which means unchangeable and read-only)
+
+// Example
+const int myNum = 15;  // myNum will always be 15
+myNum = 10;  // error: assignment of read-only variable 'myNum'
+
+const int minutesPerHour = 60;
+const float PI = 3.14;
