0b5012f0c062c11af9ea214418d51561a8262779
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-07 08:38:27 +0500

    Operators

---
+++ 9-Operators/1-Arithmetic operators.cpp
@@ -0,0 +1,15 @@
+// Arithmetic operators are used to perform common mathematical operations
+
+// Different types of Arithmetic operators
+// +	Addition	Adds together two values	x + y	
+// -	Subtraction	Subtracts one value from another	x - y	
+// *	Multiplication	Multiplies two values	x * y	
+// /	Division	Divides one value by another	x / y	
+// %	Modulus	Returns the division remainder	x % y	
+// ++	Increment	Increases the value of a variable by 1	++x	
+// --	Decrement	Decreases the value of a variable by 1	--x
+
+// Example
+int sum1 = 100 + 50;        // 150 (100 + 50)
+int sum2 = sum1 + 250;      // 400 (150 + 250)
+int sum3 = sum2 + sum2;     // 800 (400 + 400)
---
+++ 9-Operators/2-Assignment operators.cpp
@@ -0,0 +1,23 @@
+// Assignment operators are used to assign values to variables.
+
+// In the example below, we use the assignment operator (=) to assign the value 10 to a variable called x
+int x = 10;
+
+// The addition assignment operator (+=) adds a value to a variable:
+
+// Example
+int x = 10;
+x += 5;
+
+// A list of all assignment operators
+=	    x = 5	    x = 5	
++=	    x += 3	    x = x + 3	
+-=	    x -= 3	    x = x - 3	
+*=	    x *= 3	    x = x * 3	
+/=	    x /= 3	    x = x / 3	
+%=	    x %= 3	    x = x % 3	
+&=	    x &= 3	    x = x & 3	
+|=	    x |= 3	    x = x | 3	
+^=	    x ^= 3	    x = x ^ 3	
+>>=	    x >>= 3	    x = x >> 3	
+<<=	    x <<= 3	    x = x << 3
