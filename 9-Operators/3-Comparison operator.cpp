9a866087cad20a012e5421ae5d48bc50c0ee7ccf
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-07 08:42:48 +0500

    Operators

---
+++ 9-Operators/3-Comparison operator.cpp
@@ -0,0 +1,18 @@
+// Comparison operators are used to compare two values (or variables). This is important in programming, because it helps us to find answers and make decisions.
+
+// The return value of a comparison is either 1 or 0, which means true (1) or false (0). These values are known as Boolean values, and you will learn more about them in the Booleans and If..Else chapter.
+
+// In the following example, we use the greater than operator (>) to find out if 5 is greater than 3
+
+// Example
+int x = 5;
+int y = 3;
+cout << (x > y); // returns 1 (true) because 5 is greater than 3
+
+// A list of all comparison operators
+==	    Equal to	    x == y	
+!=	    Not equal	    x != y	
+>	    Greater than	x > y	
+<	    Less than	    x < y	
+>=	    Greater than or equal to	    x >= y	
+<=	    Less than or equal to	    x <= y
