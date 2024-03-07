d25a9fce2fc623799f0223a45d9dc1eebd637beb
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-07 08:47:13 +0500

    Operators

---
+++ 9-Operators/4-Logical operators.cpp
@@ -0,0 +1,19 @@
+// As with comparison operators, you can also test for true (1) or false (0) values with logical operators.
+
+// Logical operators are used to determine the logic between variables or values
+
+&& 	Logical and	Returns true if both statements are true	x < 5 &&  x < 10	
+|| 	Logical or	Returns true if one of the statements is true	x < 5 || x < 4	
+!	Logical not	Reverse the result, returns false if the result is true	!(x < 5 && x < 10)
+
+if(10>5 && 5<10){
+    // then do something...
+}
+
+if(10>5 || 5<10){
+    // then do something...
+}
+
+if(!10>5){
+    // then do something...
+}
