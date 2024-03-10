beb37713432c1df5f00132c183cf25a42ce1df00
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-10 23:15:04 +0500

    Input strings

---
+++ 10-Strings/8-Input strings.cpp
@@ -0,0 +1,32 @@
+// It is possible to use the extraction operator >> on cin to store a string entered by a user:
+
+// Example
+string firstName;
+cout << "Type your first name: ";
+cin >> firstName; // get user input from the keyboard
+cout << "Your name is: " << firstName;
+
+// Type your first name: John
+// Your name is: John
+// However, cin considers a space (whitespace, tabs, etc) as a terminating character, which means that it can only store a 
+// single word (even if you type many words)
+
+// Example
+string fullName;
+cout << "Type your full name: ";
+cin >> fullName;
+cout << "Your name is: " << fullName;
+
+// Type your full name: John Doe
+// Your name is: John
+
+// That's why, when working with strings, we often use the getline() function to read a line of text. It takes cin as the first parameter, and the string variable as second:
+
+// Example
+string fullName;
+cout << "Type your full name: ";
+getline (cin, fullName);
+cout << "Your name is: " << fullName;
+
+// Type your full name: John Doe
+// Your name is: John Doe
