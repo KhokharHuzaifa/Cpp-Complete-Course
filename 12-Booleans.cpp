af03f074e3fe9a2012733800e2ce62a02c743e22
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-19 23:19:17 +0500

    Booleans

---
+++ 12-Booleans.cpp
@@ -0,0 +1,13 @@
+// Very often, in programming, you will need a data type that can only have one of two values, like:
+
+// YES / NO
+// ON / OFF
+// TRUE / FALSE
+// For this, C++ has a bool data type, which can take the values true (1) or false (0).
+
+bool isCodingFun = true;
+bool isFishTasty = false;
+cout << isCodingFun;  // Outputs 1 (true)
+cout << isFishTasty;  // Outputs 0 (false)
+
+// From the example above, you can read that a true value returns 1, and false returns 0.
