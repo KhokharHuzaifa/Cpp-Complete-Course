77051a2c2199b4e2db1ce3c719e05efdb97f40a3
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-19 23:25:22 +0500

    Condition statements

---
+++ 15-Conditions/1-If statement.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+//    Use the if statement to specify a block of C++ code to be executed if a condition is true.
+
+// Syntax
+if (condition) {
+  // block of code to be executed if the condition is true
+}
+
+// Example
+if (20 > 18) {
+  cout << "20 is greater than 18";
+}
+
+int x = 20;
+int y = 18;
+if (x > y) {
+  cout << "x is greater than y";
+}
+
+// Example explained
+// In the example above we use two variables, x and y, to test whether x is greater than y (using the > operator). 
+// As x is 20, and y is 18, and we know that 20 is greater than 18, we print to the screen that "x is greater than y".
+}
