ae843e9cc06b91f5204ca443ebb4bf7a721b1848
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-19 23:20:56 +0500

    Boolean expressions

---
+++ 13-Boolean expressions.cpp
@@ -0,0 +1,8 @@
+// A Boolean expression returns a boolean value that is either 1 (true) or 0 (false).
+
+int x = 10;
+int y = 9;
+cout << (x > y); // returns 1 (true), because 10 is higher than 9
+cout << (10 > 9); // returns 1 (true), because 10 is higher than 9
+cout << (x == 10);  // returns 1 (true), because the value of x is equal to 10
+cout << (10 == 15);  // returns 0 (false), because 10 is not equal to 15
