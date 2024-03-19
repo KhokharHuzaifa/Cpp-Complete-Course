73c26d15c1aa74a3389bc746e830bcf5e4518498
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-19 23:22:18 +0500

    Boolean program

---
+++ 14-Boolean program.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int myAge = 25;
+int votingAge = 18;
+
+if (myAge >= votingAge) {
+  cout << "Old enough to vote!";
+} else {
+  cout << "Not old enough to vote.";
+}
+
+// Outputs: Old enough to vote!
+}
+
+// Output "Old enough to vote!" if myAge is greater than or equal to 18. Otherwise output "Not old enough to vote."
