4df7e23289f466ac69b6f39076938063b17c724e
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-27 14:11:51 +0500

    Conditional statements

---
+++ 15-Conditions/3-Ternary operator.cpp
@@ -0,0 +1,9 @@
+// It can be used to replace multiple lines of code with a single line. It is often used to replace simple 
+// if else statements
+// Syntax
+variable = (condition) ? expressionTrue : expressionFalse;
+
+// Example
+int time = 20;
+string result = (time < 18) ? "Good day." : "Good evening.";
+cout << result;
