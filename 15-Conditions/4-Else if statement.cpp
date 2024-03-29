cc0710c8078bd891a9babfcba3b369fe830ae1d5
Huzaifa Zafar <creativehuzaifa97@gmail.com>
2024-03-29 23:29:38 +0500

    Conditional statements

---
+++ 15-Conditions/4-Else if statement.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+// Use the else if statement for multiple conditions
+
+// Syntax
+if (condition) {
+  // block of code to be executed if the condition is true
+} else if(condition) {
+  // block of code to be executed if the condition is true
+}
+else{
+      // block of code to be executed if the condition is false
+}
+
