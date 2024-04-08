d10de27810ac4619644b5716e2475bc0c11eda9e
Huzaifa <creativehuzaifa97@gmail.com>
2024-04-08 23:23:36 +0500

    Conditional statements

--- 15-Conditions/5-Switch statement.cpp
+++ 15-Conditions/5-Switch statement.cpp
@@ -18,3 +18,31 @@ switch(expression) {
 // If there is a match, the associated block of code is executed
 // The break and default keywords are optional, and will be described later in this chapter
 // The example below uses the weekday number to calculate the weekday name:
+
+// Example 
+
+int day = 4;
+switch (day) {
+  case 1:
+    cout << "Monday";
+    break;
+  case 2:
+    cout << "Tuesday";
+    break;
+  case 3:
+    cout << "Wednesday";
+    break;
+  case 4:
+    cout << "Thursday";
+    break;
+  case 5:
+    cout << "Friday";
+    break;
+  case 6:
+    cout << "Saturday";
+    break;
+  case 7:
+    cout << "Sunday";
+    break;
+}
+// Outputs "Thursday" (day 4)
