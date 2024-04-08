9df5a14a35dbf539a1bccb05c1c283b4dc4da10f
Huzaifa <creativehuzaifa97@gmail.com>
2024-04-08 23:22:25 +0500

    Conditional statements

--- 15-Conditions/5-Switch statement.cpp
+++ 15-Conditions/5-Switch statement.cpp
@@ -1,3 +1,13 @@
 Use the switch statement to select one of many code blocks to be executed.
 
 Syntax
+switch(expression) {
+  case x:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+  default:
+    // code block
+}
