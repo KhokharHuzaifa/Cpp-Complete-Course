3bc5c85f85396df44de9af12b7c029dafca78029
Huzaifa <creativehuzaifa97@gmail.com>
2024-04-10 23:11:46 +0500

    Loops

---
+++ 16-Loops/1-For loop.cpp
@@ -0,0 +1,12 @@
+// Syntax
+for (statement 1; statement 2; statement 3) {
+  // code block to be executed
+}
+
+// Statement 1 is executed (one time) before the execution of the code block.
+
+// Statement 2 defines the condition for executing the code block.
+
+// Statement 3 is executed (every time) after the code block has been executed.
+
+// The example below will print the numbers 0 to 4:
