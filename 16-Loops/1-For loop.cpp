b188a22f69eb9f7aeaca94f620449b3a1831dcfe
Huzaifa <creativehuzaifa97@gmail.com>
2024-04-10 23:12:53 +0500

    Loops

--- 16-Loops/1-For loop.cpp
+++ 16-Loops/1-For loop.cpp
@@ -11,7 +11,14 @@ for (statement 1; statement 2; statement 3) {
 
 // The example below will print the numbers 0 to 4:
 
-Example
+// Example
 for (int i = 0; i < 5; i++) {
   cout << i << "\n";
 }
+
+// Example explained
+// Statement 1 sets a variable before the loop starts (int i = 0).
+
+// Statement 2 defines the condition for the loop to run (i must be less than 5). If the condition is true, the loop will start over again, if it is false, the loop will end.
+
+// Statement 3 increases a value (i++) each time the code block in the loop has been executed.
