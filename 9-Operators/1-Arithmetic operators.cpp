8c621dae38e0d4e991a1ff59c0abbae51c6483fd
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-05-01 20:33:38 +0500

    Update 1-Arithmetic operators.cpp

--- 9-Operators/1-Arithmetic operators.cpp
+++ 9-Operators/1-Arithmetic operators.cpp
@@ -9,7 +9,7 @@
 // ++	Increment	Increases the value of a variable by 1	++x	
 // --	Decrement	Decreases the value of a variable by 1	--x
 
-// Exa
+// Exam
 int sum1 = 100 + 50;        // 150 (100 + 50)
 int sum2 = sum1 + 250;      // 400 (150 + 250)
 int sum3 = sum2 + sum2;     // 800 (400 + 400)
