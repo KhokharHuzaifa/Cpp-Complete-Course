4beb8080f56d7696f56b57aed0a60465b58d3364
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-05-21 23:44:15 +0500

    Update 1-For loop.cpp

--- 16-Loops/1-For loop.cpp
+++ 16-Loops/1-For loop.cpp
@@ -3,7 +3,7 @@ for (statement 1; statement 2; statement 3) {
   // code block to be executed
 }
 
-// Statement 1 is executed (one time) before the execution of the code block.
+// Sta 1 is executed (one time) before the execution of the code block.
 
 // Statement 2 defines the condition for executing the code block.
 
