21ffa8c6e81e5ad793f1bccf1575d135e3161c0c
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-05-24 23:56:19 +0500

    Update 2-Else statement.cpp

--- 15-Conditions/2-Else statement.cpp
+++ 15-Conditions/2-Else statement.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
 // Use the else statement to specify a block of code to be executed if the condition is false
 
-// Sy
+// Syn
 if (condition) {
   // block of code to be executed if the condition is true
 } else {
