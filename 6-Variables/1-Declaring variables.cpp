023088ba9c0c78846823b2557a26340ffbce7bfd
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-05-26 23:09:47 +0500

    Update 1-Declaring variables.cpp

--- 6-Variables/1-Declaring variables.cpp
+++ 6-Variables/1-Declaring variables.cpp
@@ -1,6 +1,6 @@
 // To create a variable, specify the type and assign it a value:
 
-// Syn
+// Synt
 // type variableName = value;
 // Example
 // Create a variable called myNum of type int and assign it the value 15:
