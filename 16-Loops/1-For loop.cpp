7abdb2a905a382649ac21f1fedb7a2d84d0e1ae0
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-06-22 23:40:48 +0500

    Update 1-For loop.cpp

--- 16-Loops/1-For loop.cpp
+++ 16-Loops/1-For loop.cpp
@@ -1,6 +1,6 @@
 // Syntax
 for (statement 1; statement 2; statement 3) {
-  // code block to be executed
+  // code b to be executed
 }
 
 // Statement 1 is executed (one time) before the execution of the code block.
