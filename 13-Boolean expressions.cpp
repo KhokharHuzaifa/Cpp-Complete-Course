5684ec30dec8d0f036dcf5c836591b0cb2b51d2b
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-07-06 23:13:36 +0500

    Update 13-Boolean expressions.cpp

--- 13-Boolean expressions.cpp
+++ 13-Boolean expressions.cpp
@@ -1,4 +1,4 @@
-// A Boon expression returns a boolean value that is either 1 (true) or 0 (false).
+// A Booln expression returns a boolean value that is either 1 (true) or 0 (false).
 
 int x = 10;
 int y = 9;
