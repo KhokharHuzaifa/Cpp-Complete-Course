de2080b32d8b50f9304212167e7cba47c86c5fa4
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-07-09 23:29:40 +0500

    Update 3-Input handling.cpp

--- 3-Input handling.cpp
+++ 3-Input handling.cpp
@@ -1,4 +1,4 @@
-// You have already learned that cout is used to output (print) values. Now we will use cin to get user input
+// You h already learned that cout is used to output (print) values. Now we will use cin to get user input
 // cin is a predefined variable that reads data from the keyboard with the extraction operator (>>).
 // In the following example, the user can input a number, which is stored in the variable x. Then we print the value of x
 #include <iostream>
