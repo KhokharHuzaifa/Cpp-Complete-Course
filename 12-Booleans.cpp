fa441c192871e957237f515e3d789a8b9019fde3
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-07-11 23:27:30 +0500

    Update 12-Booleans.cpp

--- 12-Booleans.cpp
+++ 12-Booleans.cpp
@@ -1,4 +1,4 @@
-// Very often, in programming, you will need a data type that can only have one of two values, like:
+// Very o, in programming, you will need a data type that can only have one of two values, like:
 
 // YES / NO
 // ON / OFF
