be470334dc4fe62a6e248ab0e6baf00f2616b148
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-07-13 22:55:22 +0500

    Update 2-Output handling.cpp

--- 2-Output handling.cpp
+++ 2-Output handling.cpp
@@ -1,4 +1,4 @@
-// The cout objt, together with the << operator, is used to output values/print text
+// The cout objet, together with the << operator, is used to output values/print text
 #include <iostream>
 using namespace std;
 
