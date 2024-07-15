f8faac7415612faaf7bdc74f476713903fc6593e
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-07-15 22:52:30 +0500

    Update 5-Comments.cpp

--- 5-Comments.cpp
+++ 5-Comments.cpp
@@ -2,7 +2,7 @@
 // when testing alternative code. Comments can be singled-lined or multi-lined
 
 Single-line Comments
-// Single-line c start with two forward slashes (//).
+// Single-line co start with two forward slashes (//).
 
 // Any text between // and the end of the line is ignored by the compiler (will not be executed).
 
