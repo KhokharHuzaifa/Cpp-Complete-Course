8ed251ddfe848c9e872223ca65414a05b4d7eb67
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-07-23 23:12:10 +0500

    Update 1-Numeric Data types.cpp

--- 8-Data types/1-Numeric Data types.cpp
+++ 8-Data types/1-Numeric Data types.cpp
@@ -1,4 +1,4 @@
-// Numeric T
+// Numeric Ty
 // Use int when you need to store a whole number without decimals, like 35 or 1000, and float or double when you need 
 // a floating point number (with decimals), like 9.99 or 3.14515
 
