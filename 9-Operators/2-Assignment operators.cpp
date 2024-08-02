4d4e8d7fb42e083ba5d0b1ff149c5a0e93fc10ed
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-08-02 23:04:08 +0500

    Update 2-Assignment operators.cpp

--- 9-Operators/2-Assignment operators.cpp
+++ 9-Operators/2-Assignment operators.cpp
@@ -9,7 +9,7 @@ int x = 10;
 int x = 10;
 x += 5;
 
-// A list of all assignment oper
+// A list of all assignment opera
 =	    x = 5	    x = 5	
 +=	    x += 3	    x = x + 3	
 -=	    x -= 3	    x = x - 3	
