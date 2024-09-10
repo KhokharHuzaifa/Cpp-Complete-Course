3ebe6cb893e8da7c69e1c01a7615f53d7179631e
Huzaifa Zafar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-09-10 23:42:07 +0500

    Update 2-Boolean Data types.cpp

--- 8-Data types/2-Boolean Data types.cpp
+++ 8-Data types/2-Boolean Data types.cpp
@@ -1,6 +1,6 @@
 // A boolean data type is declared with the bool keyword and can only take the values true or false.
 
-// When the va is returned, true = 1 and false = 0
+// When the val is returned, true = 1 and false = 0
 
 // Example
 bool isCodingFun = true;
